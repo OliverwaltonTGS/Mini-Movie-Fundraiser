b635f992f74be06b11c4adfc41ec55736e3a74c2
OliverwaltonTGS <22097@tgs.school.nz>
2025-06-09 11:55:43 +1200

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,11 @@
+# functions go here 
+
+# main routine goes here 
+instructions = input("Would you like Instructions? ")
+
+if instructions == "yes":
+    print("The insturctions")
+elif instructions == "no":
+    pass
+else:
+    print("Please Enter either yes or no")
